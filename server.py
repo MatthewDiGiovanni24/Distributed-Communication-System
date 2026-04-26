531b2b022b41f0bc8884a6b4d808c97dbead4091
Hmullen1 <hmullen@villanova.edu>
2026-04-26 19:18:12 -0400

    Added inter-server message replication and peer synchronization

--- server.py
+++ server.py
@@ -20,12 +20,19 @@ s.bind(("", port))
 # set socket to listen
 s.listen()
 
+peer_ports = {
+    1: [1002, 1003],
+    2: [1001, 1003],
+    3: [1001, 1002]
+}
+
 clients = []
 usernames = {}
 message_history = []   # stores all messages
+seen_messages = set()
 lock = threading.Lock()
 
-server_id = "SERVER-1"
+server_id = "SERVER-" + str(server_number)
 
 leader_exists = False
 server_role = "FOLLOWER"
@@ -44,6 +51,16 @@ def broadcast(message, _client=None):
                 pass
 
 
+def send_to_peers(message):
+    for peer_port in peer_ports[server_number]:
+        try:
+            peer = socket.socket()
+            peer.connect(("127.0.0.1", peer_port))
+            peer.send(("SERVERMSG|" + message).encode())
+            peer.close()
+        except:
+            pass
+
 # heartbeat from leader every 5 seconds
 def heartbeat():
     announced = False
@@ -53,6 +70,7 @@ def heartbeat():
 
         if server_role == "LEADER" and not announced:
             broadcast("[SYSTEM] " + server_id + " is now active as LEADER")
+            print(server_id + " is LEADER")
             announced = True
 
         if server_role != "LEADER":
@@ -77,9 +95,13 @@ def handle_client(client):
             formatted = username + ": " + text
 
             with lock:
-                message_history.append(formatted)
+                if formatted not in seen_messages:
+                    seen_messages.add(formatted)
+                    message_history.append(formatted)
 
+            print(formatted)
             broadcast(formatted)
+            send_to_peers(formatted)
 
         except:
             if client in clients:
@@ -89,39 +111,73 @@ def handle_client(client):
                 left_user = usernames[client]
                 del usernames[client]
 
-                broadcast("[SYSTEM] " + left_user + " disconnected")
+                msg = "[SYSTEM] " + left_user + " disconnected"
+
+                print(msg)
+                broadcast(msg)
+                send_to_peers(msg)
 
             client.close()
             break
 
-heartbeat_thread = threading.Thread(
-    target=heartbeat,
-    daemon=True
-)
+def handle_connection(conn):
+    try:
+        first = conn.recv(1024).decode().strip()
+
+        if first.startswith("SERVERMSG|"):
+            msg = first.replace("SERVERMSG|", "", 1)
+
+            with lock:
+                if msg not in seen_messages:
+                    seen_messages.add(msg)
+                    message_history.append(msg)
+
+                    print("[SYNC] " + msg)
+                    broadcast(msg)
+
+            conn.close()
+            return
+
+        username = first
+
+        usernames[conn] = username
+        clients.append(conn)
+
+        join_msg = "[SYSTEM] " + username + " joined the chat"
+
+        print(join_msg)
+
+        with lock:
+            if join_msg not in seen_messages:
+                seen_messages.add(join_msg)
+                message_history.append(join_msg)
+
+        broadcast(join_msg)
+        send_to_peers(join_msg)
+
+        with lock:
+            for msg in message_history:
+                conn.send((msg + "\n").encode())
+
+        conn.send(
+            ("[SYSTEM] Connected to " +
+             server_id +
+             " (" +
+             server_role +
+             ")\n").encode()
+        )
+
+        thread = threading.Thread(target=handle_client, args=(conn,))
+        thread.start()
+
+    except:
+        conn.close()
+
+heartbeat_thread = threading.Thread(target=heartbeat, daemon=True)
 heartbeat_thread.start()
+
+print(server_id + " running on port " + str(port))
 
 while True:
     client, address = s.accept()
-    print("Client Connected")
-
-    username = client.recv(1024).decode().strip()
-    usernames[client] = username
-
-    clients.append(client)
-
-    broadcast("[SYSTEM] " + username + " joined the chat")
-
-    with lock:
-        for msg in message_history:
-            client.send(msg.encode())
-
-    client.send(
-        ("[SYSTEM] Connected to " +
-         server_id +
-         " (" +
-         server_role +
-         ")\n").encode()
-    )
-
-    thread = threading.Thread(target=handle_client, args=(client,))
-    thread.start()
+    threading.Thread(target=handle_connection, args=(client,)).start()
