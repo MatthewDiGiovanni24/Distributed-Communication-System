729a870a38244e60c27a9bdc9f523bded0ba8af0
Hmullen1 <hmullen@villanova.edu>
2026-04-26 19:18:49 -0400

    Added automatic server reconnect

--- client.py
+++ client.py
@@ -1,37 +1,71 @@
 import socket
 import threading
+import time
 
-s = socket.socket()
+servers = [
+    ("127.0.0.1", 1001),
+    ("127.0.0.1", 1002),
+    ("127.0.0.1", 1003)
+]
 
-port = 1001
+username = input("Enter username: ")
+s = None
+current_server = 0
 
-# connect to server on locally
-s.connect(("127.0.0.1", port))
 
-username = input("Enter your username: ")
-s.send(username.encode())
+def connect():
+    global s, current_server
 
-def read():
     while True:
         try:
-            message = s.recv(1024).decode()
-            print(message)
+            host, port = servers[current_server]
+            print(f"Connecting to {port}...")
+
+            s = socket.socket()
+            s.connect((host, port))
+            s.send(username.encode())
+
+            print(f"Connected to server {port}")
+            return
+
         except:
-            print("Disconnected from server")
-            break
+            print("Failed.")
+
+            current_server = (current_server + 1) % len(servers)
+            time.sleep(2)
+
+
+def read():
+    global s
+
+    while True:
+        try:
+            msg = s.recv(1024).decode()
+
+            if not msg:
+                raise Exception()
+
+            print(msg)
+
+        except:
+            print("Server lost. Reconnecting...")
+            connect()
 
 
 def write():
+    global s
+
     while True:
         try:
             msg = input()
             s.send(msg.encode())
+
         except:
-            break
+            print("Send failed. Reconnecting...")
+            connect()
 
 
-receive_thread = threading.Thread(target=read)
-receive_thread.start()
+connect()
 
-write_thread = threading.Thread(target=write)
-write_thread.start()
+threading.Thread(target=read, daemon=True).start()
+threading.Thread(target=write).start()
